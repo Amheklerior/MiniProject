9318a08b03916da4acd434b0b16ce90b243d9705
Andrea <andrea.amato.89@gmail.com>
2020-06-01 15:08:51 +0100

    Imports card images and defines a card-image-provider

---
+++ mini-project/Assets/Code/CardImageProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amheklerior.Solitaire {
+    
+    [CreateAssetMenu(menuName = "Solitaire/CardImageProvider")]
+    public class CardImageProvider : ScriptableObject {
+
+        #region Inspector Interface
+        [SerializeField] private Sprite[] _heartsCards = new Sprite[Deck.CARDS_PER_SEED];
+        [SerializeField] private Sprite[] _clubsCards = new Sprite[Deck.CARDS_PER_SEED];
+        [SerializeField] private Sprite[] _squaresCards = new Sprite[Deck.CARDS_PER_SEED];
+        [SerializeField] private Sprite[] _spadesCards = new Sprite[Deck.CARDS_PER_SEED];
+        [SerializeField] private Sprite _back;
+        #endregion
+
+        private Dictionary<Seed, Sprite[]> _sprites; 
+
+        private void OnEnable() => _sprites = new Dictionary<Seed, Sprite[]>() {
+            [Seed.HEARTS] = _heartsCards,
+            [Seed.CLUBS] = _clubsCards,
+            [Seed.SQUARES] = _squaresCards,
+            [Seed.SPADES] = _spadesCards
+        };
+
+        public Sprite GetFrontImageFor(Seed seed, Number number) => _sprites[seed][(int) number];
+
+        public Sprite BackImage => _back;
+
+    }
+}
---
+++ mini-project/Assets/Code/Seed.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Amheklerior.Solitaire {
+
+    public enum Seed {
+        HEARTS = 0,
+        CLUBS = 1,
+        SQUARES = 2,
+        SPADES = 3
+    }
+
+}
