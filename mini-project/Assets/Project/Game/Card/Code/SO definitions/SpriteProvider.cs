b220878e510c813c6c6b7c50da89b9278ed4b517
Andrea <andrea.amato.89@gmail.com>
2020-06-07 13:34:59 +0100

    Imported card sprites and created the sprite provider scriptable objects

---
+++ mini-project/Assets/Project/Game/Card/Code/SO definitions/SpriteProvider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Amheklerior.Solitaire {
+
+    [CreateAssetMenu(menuName = "Solitaire/Card/Sprite Provider")]
+    public class SpriteProvider : ScriptableObject {
+
+        [SerializeField] private Sprite[] _sprites;
+
+        public Sprite Get(int index) => _sprites?[index];
+
+    }
+}
